0c3abba60eefef42d95f85ad539dc378537a58c6
it22317780 <gimhanii2002@gmail.com>
2024-06-19 11:36:52 +0530

    Initial commit

---
+++ Project/src/main/java/com/customer/LoginServlet.java
@@ -0,0 +1,62 @@
+package com.customer;
+
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession(); //ADDED
+		PrintWriter out = response.getWriter();
+		response.setContentType("text/html");
+		
+		String userName = request.getParameter("username");
+		String password = request.getParameter("password");
+		boolean isTrue;
+		
+		isTrue = CustomerDBUtil.validate(userName, password);
+		
+		if (isTrue == true) {
+			List<Customer> cusDetails = CustomerDBUtil.getCustomer(userName);
+			request.setAttribute("cusDetails", cusDetails);
+			
+			String usernameFromList = getUsernameFromList(cusDetails); // ADDED
+			
+			//RequestDispatcher dis = request.getRequestDispatcher("useraccount.jsp");
+			session.setAttribute("name", usernameFromList); //ADDED
+			RequestDispatcher dis = request.getRequestDispatcher("Home.jsp");
+			dis.forward(request, response);
+		} else {
+			out.println("<script type='text/javascript'>");
+			out.println("alert('Your username or password is incorrect');");
+			out.println("location='Login.jsp'");
+			out.println("</script>");
+		}
+		
+	}
+
+
+	private String getUsernameFromList(List<Customer> cusDetails) { //ADDED
+        if (!cusDetails.isEmpty()) {								//ADDED
+            // Assuming the Customer class has a getUsername() method //ADDED
+            return cusDetails.get(0).getUserName();						//ADDED
+        }															//ADDED
+        return null;												//ADDED
+	}																//ADDED
+
+}
